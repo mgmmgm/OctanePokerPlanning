79e8e7110ed466275f8c2a0a1b45d2f503fd4498
Moshe Golan <moshe.golan@hp.com>
2016-06-27 13:28:30 +0300

    create game page

--- client/app/views/game/game.js
+++ client/app/views/game/game.js
@@ -1,15 +1,49 @@
 (function() {
   'use strict';
 
-  var gameModule = angular.module('opp.game', []);
+  var gameModule = angular.module('opp.game', ['opp.core']);
 
-  gameModule.controller('GameCtrl', ['$scope', '$state', function($scope, $state) {
+  gameModule.controller('GameCtrl', ['$scope', '$state', 'CONSTS', function($scope, $state, CONSTS) {
 
     $scope.gameName = 'game22';
 
     $scope.tableName = $state.params.tableName;
-    $scope.cardsValue = $state.params.cardsValue;
+    //$scope.cards = $state.params.cardsValue;
+    $scope.cards = CONSTS.CARDS_TYPES.SEQUENTIAL;
     $scope.release = $state.params.release;
+    $scope.sprint = 'sprint 1';
+    $scope.team = 'team1';
+    $scope.userstory = "user storyyyyyyyyyyyyyyy";
+    $scope.players = [
+      {
+        name: 'player 1',
+        voteValue: null
+      },
+      {
+        name: 'player 2',
+        voteValue: null
+      },
+      {
+        name: 'player 3',
+        voteValue: null
+      },
+      {
+        name: 'player 4',
+        voteValue: null
+      },
+      {
+        name: 'player 5',
+        voteValue: null
+      },
+    ];
+
+    $scope.disableOtherCards = function(selectedCard) {
+      angular.forEach($scope.cards, function(card) {
+        if (card.value !== selectedCard.value) {
+          card.isEnable = false;
+        }
+      })
+    }
 
   }]);
 
