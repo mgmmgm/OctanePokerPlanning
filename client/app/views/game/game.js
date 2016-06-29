ebedff7fdabd6109dfebbeda1e97934135f5897e
Moshe Golan <moshe.golan@hp.com>
2016-06-29 11:22:05 +0300

    skip user story and reset votes

--- client/app/views/game/game.js
+++ client/app/views/game/game.js
@@ -5,6 +5,8 @@
 
   gameModule.controller('GameCtrl', ['$scope', '$state', '$uibModal', 'gameSvc', 'toastSvc', 'CONSTS', function($scope, $state, $uibModal, gameSvc, toastSvc, CONSTS) {
 
+    var selectedUserstoryIndex = 0;
+
     function init() {
       gameSvc.getGameById($state.params.tableId).then(
         function(result) {
@@ -14,7 +16,22 @@
           $scope.releaseId = result.data.releaseId;
           $scope.sprintName = 'sprint 1';
           $scope.teamName = 'team1';
-          $scope.userstory = "user storyyyyyyyyyyyyyyy";
+          $scope.userstories = [
+            {
+              id: '1',
+              name: 'user story 11111'
+            },
+            {
+              id: '2',
+              name: 'user story 22'
+            },
+            {
+              id: '3',
+              name: 'user story 3333'
+            }
+          ];
+
+          $scope.selectedUserstory = $scope.userstories[selectedUserstoryIndex];
           $scope.players = result.data.players;
           $scope.linkToGame = result.data.linkToGame;
           $scope.tableId = $state.params.tableId;
@@ -22,38 +39,11 @@
       )
     }
 
-    //$scope.players = [
-    //  {
-    //    name: 'player 1',
-    //    voteValue: null,
-    //    isOwner: false
-    //  },
-    //  {
-    //    name: 'player 2',
-    //    voteValue: 10,
-    //    isOwner: true
-    //  },
-    //  {
-    //    name: 'player 3',
-    //    voteValue: 8,
-    //    isOwner: false
-    //  },
-    //  {
-    //    name: 'player 4',
-    //    voteValue: null,
-    //    isOwner: false
-    //  },
-    //  {
-    //    name: 'player 5',
-    //    voteValue: 15,
-    //    isOwner: false
-    //  }
-    //];
-
     $scope.disableOtherCards = function(selectedCard) {
         $scope.selectedValue = selectedCard.value;
       angular.forEach($scope.cards, function(card) {
         if (card.value !== selectedCard.value) {
+          card.isSelected = false;
           card.isEnable = false;
         }
       })
@@ -63,6 +53,11 @@
          gameSvc.updateStory(2463, $scope.selectedValue);
       };
 
+    $scope.skipUserstory = function() {
+      selectedUserstoryIndex++;
+      $scope.selectedUserstory = $scope.userstories[selectedUserstoryIndex];
+      resetVotes();
+    };
 
     $scope.finishVoting = function() {
       console.log($scope.players);
@@ -83,6 +78,15 @@
         console.log(data);
       });
     };
+
+    function resetVotes() {
+      angular.forEach($scope.cards, function(card) {
+          card.isEnable = true;
+        });
+      angular.forEach($scope.players, function(player) {
+        player.voteValue = null;
+      });
+    }
 
 
     $scope.addPlayer = function() {
